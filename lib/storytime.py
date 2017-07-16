80fd203975b0937f8c5fe372872ea2aa2afe0d0c
t3kt <tommy.etkin@gmail.com>
2017-07-15 21:40:29 -0700

    added segment player component

--- lib/storytime.py
+++ lib/storytime.py
@@ -85,31 +85,39 @@ class StoryDbManager(base.Extension):
 
 	def BuildStoryTable(self, dat):
 		dat.clear()
-		dat.appendRow(['id', 'teller', 'story', 'label', 'duration', 'segmentcount'])
+		dat.appendRow(['id', 'teller', 'story', 'label', 'duration', 'segmentcount', 'vidfile'])
 		for teller in self.Tellers:
 			for story in teller.stories.values():
-				dat.appendRow([
-					'{0}/{0}'.format(teller.name, story.name),
-					teller.name,
-					story.name,
-					story.label,
-					story.duration,
-					len(story.segments),
-				])
+				row = dat.numRows
+				dat.appendRow([])
+				dat[row, 'id'] = '{0}/{1}'.format(teller.name, story.name)
+				dat[row, 'teller'] = teller.name
+				dat[row, 'story'] = story.name
+				dat[row, 'duration'] = story.duration
+				dat[row, 'segmentcount'] = len(story.segments)
+				dat[row, 'vidfile'] = story.videofile
 
 	def BuildSegmentTable(self, dat):
 		dat.clear()
-		dat.appendRow(['id', 'teller', 'story', 'index', 'start', 'end', 'duration', 'text'])
+		dat.appendRow([
+			'id', 'teller', 'story', 'index',
+			'start', 'end', 'duration',
+			'start_fraction', 'end_fraction',
+			'text', 'vidfile'
+		])
 		for teller in self.Tellers:
 			for story in teller.stories.values():
 				for index, segment in enumerate(story.segments):
 					row = dat.numRows
 					dat.appendRow([])
-					dat[row, 'id'] = '{0}/{0}/{0}'.format(teller.name, story.name, index)
+					dat[row, 'id'] = '{0}/{1}/{2}'.format(teller.name, story.name, index)
 					dat[row, 'teller'] = teller.name
 					dat[row, 'story'] = story.name
 					dat[row, 'index'] = index
 					dat[row, 'start'] = segment.start
 					dat[row, 'end'] = segment.end
+					dat[row, 'start_fraction'] = segment.start / story.duration
+					dat[row, 'end_fraction'] = segment.end / story.duration
 					dat[row, 'duration'] = segment.duration
 					dat[row, 'text'] = segment.text
+					dat[row, 'vidfile'] = story.videofile
