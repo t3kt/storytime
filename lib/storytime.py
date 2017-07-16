74dd6f7941cfb2a6ea9045ae5819cab3123b1df8
t3kt <tommy.etkin@gmail.com>
2017-07-15 21:41:26 -0700

    removed deprecated stuff

--- lib/storytime.py
+++ lib/storytime.py
@@ -13,44 +13,6 @@ from storytimedb import *
 if False:
 	from _stubs import *
 
-import pysrt
-# from moviepy.editor import *
-import datetime
-
-def _SrtTimeToSeconds(t):
-	return (t.to_time() - datetime.time()).total_seconds()
-
-class SubtitleLoader(base.Extension):
-	def __init__(self, comp):
-		super().__init__(comp)
-
-	def _LoadSubFile(self):
-		path = self.comp.par.Subfile.eval()
-		if path:
-			return pysrt.open(path)
-		textdat = self.comp.par.Textdat.eval()
-		if not textdat or not textdat.text:
-			return None
-		return pysrt.from_string(textdat.text)
-
-	def FillCaptionTable(self, dat):
-		dat.clear()
-		dat.appendRow(['start', 'end', 'duration', 'start_time', 'end_time', 'duration_time', 'clean_text', 'raw_text'])
-		subfile = self._LoadSubFile()
-		if not subfile:
-			return
-		for item in subfile:
-			dat.appendRow([
-				item.start.ordinal / 1000,
-				item.end.ordinal / 1000,
-				item.duration.ordinal / 1000,
-				item.start,
-				item.end,
-				item.duration,
-				item.text_without_tags,
-				item.text,
-			])
-
 class StoryDbManager(base.Extension):
 	def __init__(self, comp):
 		super().__init__(comp)
