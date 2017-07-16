a1476094043e2f1feb9a855cd6d0a5dad149a5a4
t3kt <tommy.etkin@gmail.com>
2017-07-15 17:23:21 -0700

    implemented subtitle loader component (currently only supports SRT)

---
+++ lib/storytime.py
@@ -0,0 +1,55 @@
+try:
+	import common_base as base
+except ImportError:
+	import base
+
+try:
+	import common_util as util
+except ImportError:
+	import util
+
+if False:
+	from _stubs import *
+
+import sys
+pysrtpath = project.folder + '/pysrt'
+if pysrtpath not in sys.path:
+	sys.path.append(pysrtpath)
+del pysrtpath
+
+import pysrt
+import datetime
+
+def _SrtTimeToSeconds(t):
+	return (t.to_time() - datetime.time()).total_seconds()
+
+class SubtitleLoader(base.Extension):
+	def __init__(self, comp):
+		super().__init__(comp)
+
+	def _LoadSubFile(self):
+		path = self.comp.par.Subfile.eval()
+		if path:
+			return pysrt.open(path)
+		textdat = self.comp.par.Textdat.eval()
+		if not textdat or not textdat.text:
+			return None
+		return pysrt.from_string(textdat.text)
+
+	def FillCaptionTable(self, dat):
+		dat.clear()
+		dat.appendRow(['start', 'end', 'duration', 'start_time', 'end_time', 'duration_time', 'clean_text', 'raw_text'])
+		subfile = self._LoadSubFile()
+		if not subfile:
+			return
+		for item in subfile:
+			dat.appendRow([
+				item.start.ordinal / 1000,
+				item.end.ordinal / 1000,
+				item.duration.ordinal / 1000,
+				item.start,
+				item.end,
+				item.duration,
+				item.text_without_tags,
+				item.text,
+			])
