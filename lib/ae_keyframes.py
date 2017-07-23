e381dce8b8ad83ff0a500a4e4a26c82e132add0e
t3kt <tommy.etkin@gmail.com>
2017-07-22 17:42:15 -0700

    got the AE keyframe parsing workinggit status

--- lib/ae_keyframes.py
+++ lib/ae_keyframes.py
@@ -1,3 +1,5 @@
+import re
+
 try:
 	import common_base as base
 except ImportError:
@@ -7,6 +9,9 @@ try:
 	import common_util as util
 except ImportError:
 	import util
+
+if False:
+	from _stubs import *
 
 # from itertools import chain, zip_longest, dropwhile, takewhile
 #
@@ -36,43 +41,95 @@ except ImportError:
 #
 # 	return [str(converting_function(line)) for line in column_data]
 
-# class AeKeyframeParser(base.Extension):
-# 	def __init__(self, comp):
-# 		super().__init__(comp)
-#
+class AeKeyframeParser(base.Extension):
+	def __init__(self, comp):
+		super().__init__(comp)
+		self.animation = comp.op('./animation')
+		self.datatable = comp.op('./key_data_table')
+		self.channels = self.animation.op('./channels')
+		self.keys = self.animation.op('./keys')
+		self.Keyframes = KeyframeSet()
+		self.LoadKeyframes()
 
-def ParseKeyframes(indat, channels, keys):
-	parser = _Parser(indat)
-	parser.Parse()
-	parser.WriteTo(channels, keys)
+	def LoadKeyframes(self):
+		self.datatable.par.loadonstart.pulse()
+		self.channels.clear(keepFirstRow=True)
+		self.keys.clear(keepFirstRow=True)
+
+		parser = _Parser(
+			self,
+			self.datatable,
+			verbose=self.comp.par.Verbose.eval())
+		parser.Parse()
+		self.Keyframes = parser.output
+		self._WriteKeyframes()
+
+	def _WriteKeyframes(self):
+		self._LogBegin('_WriteKeyframes()')
+		try:
+			self.animation.par.end = self.Keyframes.maxframe
+			channels = self.channels
+			keys = self.keys
+			channels.clear(keepFirstRow=True)
+			keys.clear(keepFirstRow=True)
+			for block in self.Keyframes.blocks:
+				blockname = _Clean(block.name)
+				for attr, frames in block.attrs.items():
+					chanid = channels.numRows
+					channels.appendRow([])
+					channels[chanid, 'name'] = '{}:{}'.format(blockname, _Clean(attr))
+					channels[chanid, 'id'] = chanid
+					channels[chanid, 'left'] = channels[chanid, 'right'] = 'hold'
+					channels[chanid, 'default'] = 0
+					channels[chanid, 'keys'] = 'keys'
+					channels[chanid, 'liner'] = 0.14
+					channels[chanid, 'lineg'] = 0.5
+					channels[chanid, 'lineb'] = 0.5
+					channels[chanid, 'picked'] = 1
+					channels[chanid, 'display'] = 1
+					channels[chanid, 'template'] = 0
+					for f, val in frames:
+						i = keys.numRows
+						keys.appendRow([])
+						keys[i, 'id'] = chanid
+						keys[i, 'x'] = f + 1
+						keys[i, 'y'] = val * self.Keyframes.pixelscale
+						keys[i, 'expression'] = 'linear()'
+		finally:
+			self._LogEnd()
 
 SANITY = 20000
 
 class _Parser:
-	def __init__(self, indat):
-		self.log = base.IndentedLogger()
+	def __init__(self, host, indat, verbose=False):
+		self.host = host
 		self.indat = indat
+		self.verbose = verbose
 		self.row = 0
-		self.numRows = indat.numRows
-		self.blocks = None
-		self.maxFrame = 0
+		self.output = KeyframeSet()
 
 	@property
 	def _State(self):
+		if not self.verbose:
+			return ''
 		cells = self.indat.row(self.row)
 		return '       | cells: {0!r}'.format([repr(c.val) for c in cells] if cells else 'None')
 
+	def _FormatEvent(self, event):
+		return '[{}] {}{}'.format(self.row, event, self._State)
+
 	def _LogBegin(self, event):
-		self.log.LogBegin('(aekeyparser)', str(self.row), '{} {}'.format(event, self._State))
+		self.host.LogBegin(self._FormatEvent(event))
 
 	def _Log(self, event):
-		self.log.LogEvent('(aekeyparser)', str(self.row), '{} {}'.format(event, self._State))
+		if self.verbose:
+			self.host.LogEvent(self._FormatEvent(event))
 
 	def _LogEnd(self, event=None):
 		if event:
-			self.log.LogEnd('(aekeyparser)', str(self.row), '{} {}'.format(event, self._State))
+			self.host.LogEnd(self._FormatEvent(event))
 		else:
-			self.log.LogEnd('', '', None)
+			self.host.LogEnd()
 
 	def _GoToNextRow(self):
 		self.row += 1
@@ -81,17 +138,32 @@ class _Parser:
 		cells = self.indat.row(self.row)
 		self._Log('current row : {0!r}'.format([repr(c.val) for c in cells] if cells else 'None'))
 
+	def _ParseHeaderAttrs(self):
+		try:
+			self._LogBegin('_ParseHeaderAttrs()')
+			compattrs = {}
+			while self.row < self.indat.numRows:
+				if self.indat[self.row, 1] == '':
+					break
+				self.row += 1
+				compattrs[self.indat[self.row, 1].val] = self.indat[self.row, 2].val
+			return compattrs
+		finally:
+			self._LogEnd()
+
 	def Parse(self):
 		self._LogBegin('Parse()')
 		try:
 			self._GoToNextRow()
 			self._GoToNextRow()
+			if self.row >= self.indat.numRows:
+				return
+			self.output = KeyframeSet(self._ParseHeaderAttrs())
 			if not self._GoToNextBlankRow(1):
 				return
 			self._GoToNextRow()
-			self.blocks = []
 			sanity = SANITY
-			while self.row < self.numRows:
+			while self.row < self.indat.numRows:
 				self._LogBegin('Parse() - scanning row {}...'.format(self.row))
 				self._LogCurrentRow()
 				try:
@@ -101,13 +173,13 @@ class _Parser:
 						raise Exception('INSANITY!!!')
 					sanity -= 1
 					block = self._ParseNextBlock()
-					self.blocks.append(block)
+					self.output.blocks.append(block)
 					if self.indat[self.row, 0] == '':
 						self._GoToNextRow()
 					if self.indat[self.row, 0] == '':
 						self._GoToNextRow()
 				finally:
-					self._LogEnd('Parse() - done scanning row {}'.format(self.row))
+					self._LogEnd()
 		finally:
 			self._LogEnd()
 
@@ -116,7 +188,7 @@ class _Parser:
 		try:
 			self._GoToNextRow()
 			sanity = SANITY
-			while self.row < self.numRows:
+			while self.row < self.indat.numRows:
 				if sanity == 0:
 					raise Exception('INSANITY!!!')
 				sanity -= 1
@@ -131,21 +203,25 @@ class _Parser:
 		self._LogBegin('_ParseNextBlock()')
 		try:
 			indat = self.indat
-			group = indat[self.row, 0].val
-			components = indat[self.row, 1].val
+			# group = indat[self.row, 0].val
+			# components = indat[self.row, 1].val
 			param = indat[self.row, 2].val
-			blockname = '{}/{}/{}'.format(group, components, param)
-			self._Log('_ParseNextBlock() - block name: {}'.format(blockname))
+			# blockname = '{}/{}/{}'.format(group, components, param)
+			# blockname = '{}/{}'.format(components, param)
+			blockname = _StripNumSuffix(param)
+			block = Block(
+				name=blockname,
+				attrnames=[
+					_CleanAttrib(indat[self.row, i].val)
+					for i in range(2, indat.numCols)
+					if indat[self.row, i].val != ''
+				])
+			self._Log('_ParseNextBlock() - block name: {}'.format(block.name))
 			self._GoToNextRow()
-			attribs = {
-				indat[self.row, i].val: []
-				for i in range(2, indat.numCols)
-				if indat[self.row, i].val != ''
-			}
-			self._Log('_ParseNextBlock() - attribs: {}'.format(attribs.keys()))
+			self._Log('_ParseNextBlock() - attrs: {}'.format(block.attrs.keys()))
 			sanity = SANITY
 			numframes = 0
-			while self.row < self.numRows:
+			while self.row < self.indat.numRows:
 				self._GoToNextRow()
 				if sanity == 0:
 					raise Exception('INSANITY!!!')
@@ -154,50 +230,50 @@ class _Parser:
 					self._Log('_ParseNextBlock() - found blank row in block. stopping')
 					break
 				f = int(indat[self.row, 1])
-				if f > self.maxFrame:
-					self.maxFrame = f
-				for i, attr in enumerate(attribs):
-					attribs[attr].append([f, float(indat[self.row, i + 2])])
+				if f > self.output.maxframe:
+					self.output.maxframe = f
+				for i, attr in enumerate(block.attrs):
+					block.attrs[attr].append([f, float(indat[self.row, i + 2])])
 				numframes += 1
-				# self._Log('attribs: {}'.format(attribs))
 			self._Log('_ParseNextBlock() - found {} frames'.format(numframes))
-			return {
-				'name': blockname,
-				'attribs': attribs,
-			}
+			return block
 		finally:
 			self._LogEnd()
 
-	def WriteTo(self, channels, keys):
-		self._LogBegin('WriteTo()')
-		try:
-			channels.clear(keepFirstRow=True)
-			keys.clear(keepFirstRow=True)
-			for block in self.blocks:
-				blockname = block['name']
-				for attr, frames in block['attribs'].items():
-					chanid = channels.numRows
-					channels.appendRow([])
-					channels[chanid, 'name'] = _Clean('{}/{}'.format(blockname, attr))
-					channels[chanid, 'id'] = chanid
-					channels[chanid, 'left'] = channels[chanid, 'right'] = 'hold'
-					channels[chanid, 'default'] = 0
-					channels[chanid, 'keys'] = 'keys'
-					channels[chanid, 'liner'] = 0.14
-					channels[chanid, 'lineg'] = 0.5
-					channels[chanid, 'lineb'] = 0.5
-					channels[chanid, 'picked'] = 0
-					channels[chanid, 'display'] = 1
-					channels[chanid, 'template'] = 0
-					for f, val in frames:
-						i = keys.numRows
-						keys.appendRow([])
-						keys[i, 'id'] = chanid
-						keys[i, 'x'] = f
-						keys[i, 'y'] = val
-						keys[i, 'expression'] = 'linear()'
-		finally:
-			self._LogEnd('WriteTo()')
+class Block:
+	def __init__(self, name, attrnames):
+		self.name = name
+		self.attrs = {
+			attr: []
+			for attr in attrnames
+		}
+
+class KeyframeSet:
+	def __init__(self, attrs=None):
+		if attrs is None:
+			attrs = {}
+		self.attrs = attrs
+		self.width = util.ParseFloat(attrs.get('Source Width'), 1)
+		self.height = util.ParseFloat(attrs.get('Source Height'), 1)
+		self.pixelscale = 1.0 / min(self.width, self.height)
+		self.fps = util.ParseFloat(attrs.get('Units Per Second'), 30)
+		self.maxframe = 0
+		self.blocks = []
+
+	def __repr__(self):
+		return 'KeyframeSet(w:{}, h:{}, fps:{}, maxframe:{}, blocks:{})'.format(
+			self.width, self.height, self.fps, self.maxframe, len(self.blocks))
 
 def _Clean(n):
 	return tdu.legalName(n)
+
+_numSuffixRx = re.compile(r'\s*#\d+')
+def _StripNumSuffix(s):
+	return _numSuffixRx.sub('', s)
+
+_pixelsSuffix = re.compile(r' pixels$')
+def _CleanAttrib(s):
+	return _pixelsSuffix.sub('', s)
+
+def _IsPixels(s):
+	return _pixelsSuffix.match(s)
