5381faf1adaa582d4f6d20bfcb98c9e270b67d07
t3kt <tommy.etkin@gmail.com>
2017-08-30 22:59:39 -0700

    implementing json output for the processor

--- processor/src/ofApp.h
+++ processor/src/ofApp.h
@@ -2,6 +2,35 @@
 
 #include "ofMain.h"
 #include "ofxFaceTracker.h"
+#include <set>
+#include <iostream>
+
+using Direction = ofxFaceTracker::Direction;
+using Feature = ofxFaceTracker::Feature;
+using Gesture = ofxFaceTracker::Gesture;
+
+class Settings {
+public:
+  bool includePoints;
+  bool includeImagePoints;
+  bool includeObjectPoints;
+  bool includeMeanObjectPoints;
+
+  bool includeMeshes;
+  bool includeImageMeshes;
+  bool includeObjectMeshes;
+  bool includeMeanObjectMeshes;
+
+  bool includeHaarRectangle;
+  bool includePosition;
+  bool includeScale;
+  bool includeOrientation;
+  bool includeRotationMatrix;
+
+  bool includeDirection;
+  bool includeFeatures;
+  bool includeGestures;
+};
 
 class ofApp : public ofBaseApp{
 
@@ -14,8 +43,10 @@ private:
   void writeHeader();
   ofJson writeFrameObj();
 
+  Settings settings;
   ofVideoPlayer video;
   ofxFaceTracker tracker;
   ofFile dataFile;
   ofJson dataFrames;
 };
+
