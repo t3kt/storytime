3bdc0ffc898e31bfe104af34023d1c99b468e084
t3kt <tommy.etkin@gmail.com>
2017-09-09 17:23:13 -0700

    fixed build error

--- processor/src/Output.cpp
+++ processor/src/Output.cpp
@@ -2,8 +2,8 @@
 #include "JsonOutput.h"
 #include <ofMain.h>
 
-std::shared_ptr<TrackingOuput>
-TrackingOuput::createOutput(const OutputSettings& settings) {
+std::shared_ptr<TrackingOutput>
+TrackingOutput::createOutput(const OutputSettings& settings) {
   switch (settings.format) {
     case OutputFormat::JSON:
       return std::make_shared<JsonTrackingOutput>(settings);
