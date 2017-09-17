41708836ee752fd4eff4e590542d76abca013ee9
tekt <tommy.etkin@gmail.com>
2017-09-17 13:56:50 -0700

    implemented the animation key writer

--- storytime/animation.py
+++ storytime/animation.py
@@ -44,16 +44,38 @@ class AnimationKeysWriter:
 	def __init__(self, path, channels):
 		self.path = path
 		self.channels = channels
+		self.outfile = None
+		self.writer = None  # type: csv.DictWriter
 
-	def WriteChannels(self, path):
-		with open(path, 'w') as outfile:
-			_initCsvDATDialect()
-			writer = csv.DictWriter(
-				outfile,
-				dialect='DAT',
-				fieldnames=[
-					'id', 'x',  'y', 'inslope', 'inaccel', 'expression',
-					'outslope', 'outaccel',
-				])
-			writer.writeheader()
-			# ..........
+	def __enter__(self):
+		self.outfile = open(self.path, 'w')
+		_initCsvDATDialect()
+		self.writer = csv.DictWriter(
+			self.outfile,
+			dialect='DAT',
+			fieldnames=[
+				'id', 'x',  'y', 'inslope', 'inaccel', 'expression',
+				'outslope', 'outaccel',
+			])
+		return None
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		result = self.outfile.__exit__(exc_type, exc_val, exc_tb)
+		self.outfile = None
+		return result
+
+	def WriteHeader(self):
+		self.writer.writeheader()
+
+	def WriteFrameFromList(self, frame, channelvals):
+		for channelindex, channelval in enumerate(channelvals):
+			self.writer.writerow({
+				'id': channelindex + 1,
+				'x': frame,
+				'y': channelval,
+				'inslope': '',
+				'inaccel': '',
+				'expression': 'linear()',
+				'outslope': '',
+				'outaccel': '',
+			})
